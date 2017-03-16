7358c884582c249bf0030122bd51d15711a3e197
hananiahhsu <hananiahhsu@126.com>
2017-03-16 12:32:44 +0800

    first commit

---
+++ GraphicsApplication/GraphicsApplication.cpp
@@ -0,0 +1,12 @@
+// GraphicsApplication.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <QtWidgets\qapplication.h>
+
+int main(int argc,char* argv[])
+{
+	QApplication app(argc,argv);
+    return app.exec();
+}
+
